96f5af404138c06087180e0c74ba458ae1ee06f0
giacomotomasi <56025014+giacomotomasi@users.noreply.github.com>
2023-03-13 18:00:22 +0100

    Unnecessary comments remuval.

--- test.py
+++ test.py
@@ -42,12 +42,6 @@ rot_mat2 = np.array([[1.0, 0.0, 0.0],
                      [0.0, 0.0, 1.0]])
 d2.setBBoxOrientation(rotation_mat=rot_mat2)
 d2.setBBoxSize(size=[0.5, 0.5, 1.0])
-
-# print("\nd1 id:", id(d1))
-# print("d2 id:", id(d2))
-# print("\n d1 pos:", d1.bounding_box.position, "\n d2 pos:", d2.bounding_box.position)
-# print("\n d1 affine_transformation id:", id(d1.affine_transform), "\n d2 affine_transformation id:", id(d2.affine_transform))
-
 d.detections.append(d1)
 d.detections.append(d2)
 visualizer = Visualizer(list_of_transformations=[frame_0, frame_1, frame_2], detection_list=d)
